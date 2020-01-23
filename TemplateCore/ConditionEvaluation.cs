ee21b4391e017f1c44db932e20fc7bd1db82853c
Ranjeeth.Pasupathi <rpasupat@in.ibm.com>
2020-01-23 02:51:06 -0800

    Conditions skeleton incremental

--- TemplateCore/ConditionEvaluation.cs
+++ TemplateCore/ConditionEvaluation.cs
@@ -20,28 +20,56 @@ namespace SmartExportTemplates.TemplateCore
 
         public bool CanEvaluate()
         {
-            //Split the conditions
-            SplitConditions();
+            //Parse Conditions
+            ParseConditions();
             //Apply condition and overwrite the boolen
-            ApplyConditions();
+            ApplyConditionsAndReplace();
             //Evaluate condition and return
             return EvaluateConditions();
         }
 
-        private void SplitConditions()
+        private void ParseConditions()
         {
+            //Split the conditions into a set of individual conditions.
+            //Example: "a>b and c==d or z<y and m==n" is stored in the below array as
+            // ["a>b", "and", "c==d", "or", "z<y", "and", "m==n"]
             this.LexParseCondList = new List<string>(Regex.Split(this.ConditionText, Constants.IF_REF_PATTERN));
-
+            //Validate if the list is and ODD number. 
+            if ((this.LexParseCondList.Count & 1) == 0)
+            {
+                throw new SmartExportException("Unsupported syntax. Check documentation: " + ConditionText);
+            }
         }
 
-        private void ApplyConditions()
+        private void ApplyConditionsAndReplace()
         {
-
+            // Evaluate conditions and store the boolean back onto the list
+            List<string> tmpList = this.LexParseCondList.Select(x => ReplaceConditions(x)).ToList();
+            this.LexParseCondList = tmpList;
         }
 
-         private bool EvaluateConditions()
+        private string ReplaceConditions(string conditionText)
         {
+            string output = conditionText;
+            //Don't replace "and/or", evaluate the rest as individual conditions
+            if (!Regex.Match(conditionText, Constants.IF_REF_PATTERN).Success)
+            {
+                output = EvaluateIndividualCondition(conditionText) ? "true" : "false";
+            }
+            return output;
+        }
+
+        private bool EvaluateIndividualCondition(string conditionText)
+        {
+            // TODO - The main implementation of evaluating a single condition goes here!
             return true;
         }
+
+        private bool EvaluateConditions()
+        {
+            //TODO - From left to right, apply the logical and/or operators on the individual condition results and return a single bool
+            return true;
+        }
+
     }
 }
