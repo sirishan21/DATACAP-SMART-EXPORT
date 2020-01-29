79d9a555643a128c9c02926ca7b7c6422ea735ca
Sirisha Sharon Nethala <sirisha.nethala@in.ibm.com>
2020-01-29 02:03:18 -0800

    Bug fixes and refactoring

--- DCOUtil/DCODataRetriever.cs
+++ DCOUtil/DCODataRetriever.cs
@@ -28,10 +28,10 @@ namespace SmartExportTemplates.DCOUtil
                     ExportCore.WriteLog(Constants.GE_LOG_PREFIX + message);
                     throw new SmartExportException(message);
                 case Constants.Document:
-                    output = getDCOValueForDocument(DCOTree, CurrentDCO.ID);
+                    output = getDCOValueForDocument(DCOTree);
                     break;
                 case Constants.Page:
-                    output = getDCOValueForPage(DCOTree, CurrentDCO.ID);
+                    output = getDCOValueForPage(DCOTree);
                     break;
                 case Constants.Field:
                     output = getDCOValueForField(DCOTree);
@@ -56,19 +56,22 @@ namespace SmartExportTemplates.DCOUtil
             DCOTree = DCOTree.Replace("[", "").Replace("]", "");
             char[] sep = { '.' };
             string[] dcoArray = DCOTree.Split(sep, 4, StringSplitOptions.None);
-            string pageID = CurrentDCO.Parent().ID;
-            string output = null;
+            TDCOLib.DCO page = CurrentDCO.Parent();
+            string pageID = page.ID;
+            TDCOLib.DCO document = page.Parent();
+            string documentID = document.ID; 
+            string output = "";
             try
             {
                 // this is to pick up the field from the right page type
-                if(dcoArray[3]== CurrentDCO.ID && dcoArray[2] == getPageType(pageID))
+                if(dcoArray[3]== CurrentDCO.ID && dcoArray[1] == document.Type && dcoArray[2] == page.Type)
                 {
-                    return getDCOValueForPage(DCOTree, CurrentDCO.Parent().ID);
+                    return CurrentDCO.Text;
                 }
                 else
                 {
-                    ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "Current field "+ CurrentDCO.ID +
-                        " is not different from required field which is" + dcoArray[3]);
+                    ExportCore.WriteLog(Constants.LOG_PREFIX + " Current field "+ CurrentDCO.ID +
+                        " is  different from required field which is" + DCOTree);
                 }
             }
             catch (Exception exp)
@@ -77,20 +80,19 @@ namespace SmartExportTemplates.DCOUtil
                 // Template in TravelDocs can have reference to a field under Flight but the current batch doesn't have 
                 // any flight related input. Alternatively, Flight and Car Rental gets processed but for the Car Rental 
                 // data output, there cannot be any Flight reference
-                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "Unable to find DCO reference for the field with ID: " + dcoArray[3]);
-                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "Error while reading DCO reference: " + exp.ToString());
+                ExportCore.WriteLog(Constants.LOG_PREFIX + " Unable to find DCO reference for the field : " + DCOTree);
+                ExportCore.WriteLog(Constants.LOG_PREFIX + " Error while reading DCO reference: " + exp.ToString());
             }
 
             return output;
         }
 
         ///       <summary>
-        ///       The method returns the value corresponding to the DCO expression specified from the current DCO for a given page.
+        ///       The method returns the value corresponding to the DCO expression specified from the current DCO.
         ///       <param name="DCOTree">DCO Expression in the format [DCO].[document_type].[page_type].[field_name]</param>
-        ///       <param name="pageID">Page ID</param>
         ///       <returns>The value corresponding to the DCO expression specified from the current DCO.</returns>
         ///       </summary>
-        public string getDCOValueForPage(string DCOTree, string pageID)
+        public string getDCOValueForPage(string DCOTree )
         {
             // DCO reference in the template file should adhere to a 4 part string [DCO.<doc_type>.<page_type>.<field_name>]
             // Parse the DCO reference and extract the page_type and field_name which can then be used to look up in the 
@@ -99,10 +101,20 @@ namespace SmartExportTemplates.DCOUtil
             char[] sep = { '.' };
             string[] dcoArray = DCOTree.Split(sep, 4, StringSplitOptions.None);
                 
-            string output = null;
+            string output = "";
             try
             {
-                output = DCO.FindChild(pageID).FindChild(dcoArray[3]).Text;
+                 
+                if (dcoArray[1] == CurrentDCO.Parent().Type && dcoArray[2] == CurrentDCO.Type)
+                {
+                    // output = DCO.FindChild(pageID).FindChild(dcoArray[3]).Text;
+                    output =CurrentDCO.FindChild(dcoArray[3]).Text;
+                }
+                else
+                {
+                    ExportCore.WriteLog(Constants.LOG_PREFIX + " The expression   " + DCOTree+ " is not valid for page  " + CurrentDCO.ID);
+                }
+                
             }
             catch (Exception exp)
             {
@@ -110,20 +122,19 @@ namespace SmartExportTemplates.DCOUtil
                 // Template in TravelDocs can have reference to a field under Flight but the current batch doesn't have 
                 // any flight related input. Alternatively, Flight and Car Rental gets processed but for the Car Rental 
                 // data output, there cannot be any Flight reference
-                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "Unable to find DCO reference for the page with ID: " + pageID);
-                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "Error while reading DCO reference: " + exp.ToString());
+                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + " Unable to find DCO reference for the page with ID: " + CurrentDCO.ID);
+                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + " Error while reading DCO reference: " + exp.ToString());
             }
 
             return output;
         }
 
         ///       <summary>
-        ///       The method returns the value corresponding to the DCO expression specified from the current DCO for a given document.
+        ///       The method returns the value corresponding to the DCO expression specified from the current DCO.
         ///       <param name="DCOTree">DCO Expression in the format [DCO].[document_type].[page_type].[field_name]</param>
-        ///       <param name="DocumentID">Docuemnt ID</param>
         ///       <returns>The value corresponding to the DCO expression specified from the current DCO.</returns>
         ///       </summary>
-        public string getDCOValueForDocument(string DCOTree, string DocumentID )
+        public string getDCOValueForDocument(string DCOTree )
         {
 
             // DCO reference in the template file should adhere to a 4 part string [DCO.<doc_type>.<page_type>.<field_name>]
@@ -134,14 +145,22 @@ namespace SmartExportTemplates.DCOUtil
             string[] dcoArray = DCOTree.Split(sep, 4, StringSplitOptions.None);
 
             // get the value of the DCO reference using the page ID and the field name
-            string output = null;
+            string output = "";
 
       
             try
             {
-                string pageID = getPageIDOfTypeInDocument(DCOTree, DocumentID, dcoArray[2]);
-                output = CurrentDCO.FindChild(pageID).FindChild(dcoArray[3]).Text;
-                
+                if(dcoArray[1] == CurrentDCO.Type)
+                {
+                    string pageID = getPageIDOfTypeInDocument(DCOTree, CurrentDCO.ID, dcoArray[2]);
+                    output = CurrentDCO.FindChild(pageID).FindChild(dcoArray[3]).Text;
+                }
+                else
+                {
+                    ExportCore.WriteLog(Constants.LOG_PREFIX + " The expression   " + DCOTree + " is not valid for document  " + CurrentDCO.ID);
+
+                }
+
             }
             catch (Exception exp)
             {
@@ -149,8 +168,8 @@ namespace SmartExportTemplates.DCOUtil
                 // Template in TravelDocs can have reference to a field under Flight but the current batch doesn't have 
                 // any flight related input. Alternatively, Flight and Car Rental gets processed but for the Car Rental 
                 // data output, there cannot be any Flight reference
-                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "Unable to find DCO reference for the document with ID: " + DocumentID);
-                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "Error while reading DCO reference: " + exp.ToString());
+                ExportCore.WriteLog(Constants.LOG_PREFIX + " Unable to find DCO reference for the document with ID: " + CurrentDCO.ID);
+                ExportCore.WriteLog(Constants.LOG_PREFIX + " Error while reading DCO reference: " + exp.ToString());
             }
 
             return output;
@@ -184,7 +203,7 @@ namespace SmartExportTemplates.DCOUtil
                     // Data to be processed is always within pages
                     if (pageList.Count == 0)
                     {
-                        ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "No pages in document: " + ID);
+                        ExportCore.WriteLog(Constants.LOG_PREFIX + " No pages in document: " + ID);
                         break;
                     }
                     foreach (XmlNode pageNode in pageList)
@@ -200,9 +219,9 @@ namespace SmartExportTemplates.DCOUtil
                         if(pageCount > 1)
                         {
                             pageID = "";
-                            string message = "More than 1 page of type " + pageType + "  found in document: " + ID +
-                                "Expression cannot be evaluated due to ambiguity. " + DCOTree;
-                            ExportCore.WriteLog(Constants.GE_LOG_PREFIX + message );
+                            string message = " More than 1 page of type " + pageType + "  found in document: " + ID +
+                                " Expression cannot be evaluated due to ambiguity. " + DCOTree;
+                            ExportCore.WriteLog(Constants.LOG_PREFIX + message );
                             throw new SmartExportException(message);
                            
                         }
@@ -214,53 +233,13 @@ namespace SmartExportTemplates.DCOUtil
             }
             if (!foundDoc)
             {
-                string message = "Document with ID " + documentID + " not  found.";
-                ExportCore.WriteLog(Constants.GE_LOG_PREFIX + message);
+                string message = " Document with ID " + documentID + " not  found.";
+                ExportCore.WriteLog(Constants.LOG_PREFIX + message);
                 throw new SmartExportException(message);
             }
             return pageID;
             
         }
-
-
-        ///       <summary>
-        ///       The method returns the page type of the specified page.
-        ///       <param name="pageID">Page ID</param>
-        ///       <returns>The page ID of the specified page typs in the specified document.</returns>
-        ///       </summary>
-        public string getPageType(string pageID)
-        {
-            string pageType ="";
-
-            XmlDocument batchXML = new XmlDocument();
-            batchXML.Load((string)Globals.Instance.GetData(Constants.GE_BATCH_XML_FILE));
-            XmlElement batchRoot = batchXML.DocumentElement;
-            XmlNodeList dcoDocumentNodes = batchRoot.SelectNodes("./D"); //Document nodes
-            foreach (XmlNode dcoDocumentNode in dcoDocumentNodes)
-            {
-                String DocumentID = ((XmlElement)dcoDocumentNode).GetAttribute("id");
-                XmlNodeList pageList = dcoDocumentNode.SelectNodes("./P");
-                // Data to be processed is always within pages
-                if (pageList.Count == 0)
-                {
-                    ExportCore.WriteLog(Constants.GE_LOG_PREFIX + "No data to proces. Processing skipped for document: " + DocumentID);
-                    continue;
-                }
-
-                foreach (XmlNode pageNode in pageList)
-                {
-                    string ID = ((XmlElement)pageNode).GetAttribute("id");                    
-                    if (pageID == ID)
-                    {
-                        pageType = pageNode.SelectSingleNode("./V[@n='TYPE']").InnerText;
-                        break;
-                    }
-                }               
-            }
-            return pageType;
-        }
-
-        
 
     }
 }
